b3f162fe44e4a129ffa96ed06ea9472e170ca6d3
Jacob Brewer <Jacob.brewer@bthree.uk>
2025-03-06 14:55:40 +0000

    gen

--- mock_Pool.go
+++ mock_Pool.go
@@ -14,7 +14,7 @@ type MockPool struct {
 	mock.Mock
 }
 
-// Conn provides a mock function with given fields:
+// Conn provides a mock function with no fields
 func (_m *MockPool) Conn() redis.Conn {
 	ret := _m.Called()
 
@@ -35,7 +35,7 @@ func (_m *MockPool) Conn() redis.Conn {
 }
 
 // Do provides a mock function with given fields: command, args
-func (_m *MockPool) Do(command string, args ...any) (any, error) {
+func (_m *MockPool) Do(command string, args ...interface{}) (interface{}, error) {
 	var _ca []interface{}
 	_ca = append(_ca, command)
 	_ca = append(_ca, args...)
@@ -45,20 +45,20 @@ func (_m *MockPool) Do(command string, args ...any) (any, error) {
 		panic("no return value specified for Do")
 	}
 
-	var r0 any
+	var r0 interface{}
 	var r1 error
-	if rf, ok := ret.Get(0).(func(string, ...any) (any, error)); ok {
+	if rf, ok := ret.Get(0).(func(string, ...interface{}) (interface{}, error)); ok {
 		return rf(command, args...)
 	}
-	if rf, ok := ret.Get(0).(func(string, ...any) any); ok {
+	if rf, ok := ret.Get(0).(func(string, ...interface{}) interface{}); ok {
 		r0 = rf(command, args...)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(any)
+			r0 = ret.Get(0).(interface{})
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(string, ...any) error); ok {
+	if rf, ok := ret.Get(1).(func(string, ...interface{}) error); ok {
 		r1 = rf(command, args...)
 	} else {
 		r1 = ret.Error(1)
@@ -68,7 +68,7 @@ func (_m *MockPool) Do(command string, args ...any) (any, error) {
 }
 
 // DoCtx provides a mock function with given fields: ctx, command, args
-func (_m *MockPool) DoCtx(ctx context.Context, command string, args ...any) (any, error) {
+func (_m *MockPool) DoCtx(ctx context.Context, command string, args ...interface{}) (interface{}, error) {
 	var _ca []interface{}
 	_ca = append(_ca, ctx, command)
 	_ca = append(_ca, args...)
@@ -78,20 +78,20 @@ func (_m *MockPool) DoCtx(ctx context.Context, command string, args ...any) (any
 		panic("no return value specified for DoCtx")
 	}
 
-	var r0 any
+	var r0 interface{}
 	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, string, ...any) (any, error)); ok {
+	if rf, ok := ret.Get(0).(func(context.Context, string, ...interface{}) (interface{}, error)); ok {
 		return rf(ctx, command, args...)
 	}
-	if rf, ok := ret.Get(0).(func(context.Context, string, ...any) any); ok {
+	if rf, ok := ret.Get(0).(func(context.Context, string, ...interface{}) interface{}); ok {
 		r0 = rf(ctx, command, args...)
 	} else {
 		if ret.Get(0) != nil {
-			r0 = ret.Get(0).(any)
+			r0 = ret.Get(0).(interface{})
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(context.Context, string, ...any) error); ok {
+	if rf, ok := ret.Get(1).(func(context.Context, string, ...interface{}) error); ok {
 		r1 = rf(ctx, command, args...)
 	} else {
 		r1 = ret.Error(1)
